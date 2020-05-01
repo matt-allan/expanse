da48554329a9f29ebd0daf51e0b6fc3dbcabfc1a
Matthew Allan <matt@mattallan.me>
2020-05-01 11:08:35 -0400

    Add Grommet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Grommet } from 'grommet';
 import './index.css';
+import * as theme from './theme.json';
 
-ReactDOM.render(<div>hello world from React! </div>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+  {/*
+    // @ts-ignore */}
+    <Grommet theme={theme}> 
+      <div>hello world from React! </div>
+    </Grommet>
+  </div>,
+  document.getElementById('root')
+);
