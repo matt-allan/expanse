78295f05eba4d569209fadb647e0b9a58118310d
Matthew Allan <matt@mattallan.me>
2020-05-02 11:50:24 -0400

    Setup clock, meter, and controls

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,30 @@ import { Play, Pause, Resume, Refresh } from "grommet-icons";
 
 import { theme } from './theme';
 
+const TIME_REGEXP = /P([0-9]+)H([0-9]+)M([0-9]+)S/;
+
+// todo: make configurable
+const MINUTES_PER_SESSION = 25;
+
 const App = () => {
-  const [value, setValue] = useState(20);
+  const [paused, setPaused] = useState(true);
 
-  const timer = useRef();
-  clearTimeout(timer.current);
-  // @ts-ignore
-  timer.current = setTimeout(() => {
-    setValue(value < 100 ? value + 8 : 20);
-  }, 2000);
+  const [secondsLeft, setSecondsLeft] = useState(MINUTES_PER_SESSION * 60);
 
-  useEffect(() => {
-    return () => {
-      clearTimeout(timer.current);
-    };
-  }, []);
+  const onChange = (time: any) => {
+    let match = TIME_REGEXP.exec(time);
+    if (!match) {
+      console.error(`Cannot parse '${time}'`);
+      return;
+    }
+    const hours = parseFloat(match[1]);
+    const minutes = parseFloat(match[2]) || 0;
+    const seconds = parseFloat(match[3]) || 0;
+
+    setSecondsLeft(
+      (hours * 60 * 60) + (minutes * 60) + seconds
+    );
+  };
 
   return (
     <div>
@@ -28,24 +37,26 @@ const App = () => {
           <Meter
             type="circle"
             background="light-2"
-            values={[{ value, color: value > 50 ? "accent-2" : "accent-1" }]}
+            max={MINUTES_PER_SESSION * 60}
+            values={[{ value: secondsLeft }]}
           />
         </Box>
         <Box align="center" justify="center" pad="small">
           <Clock
+            key={secondsLeft == MINUTES_PER_SESSION * 60 ? 'restarted' : 'running'}
             type="digital"
-            time="T00:25:00"
-            precision="seconds"
-            run="backward"
+            time={`PT0H${MINUTES_PER_SESSION}M0S`}
+            run={paused ? false : 'backward'}
+            onChange={onChange}
             alignSelf="center"
             size="xlarge"
             margin="xlarge"
             a11yTitle="countdown timer until next break"
           />
-        </Box>
-        <Box direction="row" justify="center" gap="medium" pad="small">
-          <Button plain={true} icon={<Refresh />} onClick={() => {}} />
-          <Button plain={true} icon={<Play />} onClick={() => {}} />
+          </Box>
+        <Box direction="row" justify="center" gap="medium" pad={{top: 'small', bottom: 'large'}}>
+          <Button plain={true} icon={<Refresh />} onClick={() => setSecondsLeft(MINUTES_PER_SESSION * 60)} />
+          <Button plain={true} icon={paused ? <Play /> : <Pause />} onClick={() => setPaused(!paused)} />
         </Box>
       </Grommet>
     </div>
