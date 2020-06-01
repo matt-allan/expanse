9a3022a261e4da9d0d7585bf5c08dc2cb6c82936
Matthew Allan <matt@mattallan.me>
2020-06-01 19:17:01 -0400

    Add a basic breathing exercise

--- src/components/Break.tsx
+++ src/components/Break.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Image } from 'grommet';
+import { Box } from 'grommet';
+
+import { Breathe } from './exercises/Breathe';
 
 const browserWindow = window.expanse.browserWindow;
 
@@ -16,16 +18,8 @@ export const Break = ({ onEnd }: BreakProps) => {
   }, []);
 
   return (
-    <React.Fragment>
-      <Box align="center" justify="center" pad="small">
-        <Image
-            fit="cover"
-            src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"
-          />
-      </Box>
-      <Box align="center" justify="center" pad="small">
-        <Button primary label="End Break" onClick={onEnd} />
-      </Box>
-    </React.Fragment>
+    <Box align="center" justify="center" pad="small" height="100vh">
+      <Breathe onEnd={onEnd} />
+    </Box>
   );
 };
---
+++ src/components/exercises/Breathe.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { defaultProps, Button } from 'grommet';
+
+// Only undefined if you overwrite the theme
+// and dont use the deepMerge function.
+const colors = defaultProps!.theme!.global!.colors!;
+
+const pulse = keyframes`
+  0% {
+    transform: scale(0.1) rotate(180deg);
+  }
+  100% {
+    transform: scale(0.1);
+  }
+}
+`;
+
+const resize = keyframes`
+  0% {
+    transform: scale(1) 
+  }
+  100% {
+    transform: scale(10) 
+  }
+`;
+
+const Container = styled.div`
+  height: 125px;
+  width: 125px;
+  animation: ${pulse} 4s ease alternate infinite;
+`;
+
+const Circle = styled.div`
+  height: 125px;
+  width: 125px;
+  border-radius: 50%;
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  opacity: 0.75;
+  animation: ${resize} 4s ease alternate infinite;
+  mix-blend-mode: screen;
+
+  &:nth-child(odd) {
+    background: ${colors['accent-1']};
+  }
+
+  &:nth-child(even) {
+    background: ${colors['accent-3']};
+  }
+
+  &:nth-child(1) {
+    transform-origin: 95% 75%;
+  }
+
+  &:nth-child(2) {
+    transform-origin: 50% 100%;
+  }
+
+  &:nth-child(3) {
+    transform-origin: 5% 75%;
+  }
+
+  &:nth-child(4) {
+    transform-origin: 95% 25%;
+  }
+
+  &:nth-child(5) {
+    transform-origin: 50% 0%;
+  }
+
+  &:nth-child(6) {
+    transform-origin: 5% 25%;
+  }
+`;
+
+type BreatheProps = {
+  onEnd: () => void;
+};
+
+export const Breathe = ({ onEnd }: BreatheProps) => {
+  const [finished, setFinished] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setFinished(true), 1000 * 60 * 2);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (finished) {
+    return (<Button primary label="Back to work" onClick={onEnd} />);
+  }
+
+  return (
+    <Container>
+      <Circle />
+      <Circle />
+      <Circle />
+      <Circle />
+      <Circle />
+      <Circle />
+    </Container>
+  );
+};
