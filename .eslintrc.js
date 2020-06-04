50d82a5e522a2495cfc93ab5bcb25354df938345
Matthew Allan <matt@mattallan.me>
2020-06-04 11:26:00 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
