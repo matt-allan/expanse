c7766718f53c3eb5b61c16b034085c071455f0f8
Matthew Allan <matt@mattallan.me>
2020-06-09 13:59:03 -0400

    Add react-hooks eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
   ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
   env: {
     browser: true,
     node: true,
